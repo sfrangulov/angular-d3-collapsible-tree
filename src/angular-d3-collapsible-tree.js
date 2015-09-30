f44bd0d60b4e577f93e82d5e4d6fb1540cb58dff
sfrangulov <sergey.frangulov@gmail.com>
2015-09-30 10:19:33 +0300

    Update angular-d3-collapsible-tree.js

--- src/angular-d3-collapsible-tree.js
+++ src/angular-d3-collapsible-tree.js
@@ -1,166 +1,230 @@
  /* jslint node: true */
  /* global angular, $, d3 */
-'use strict';
+ 'use strict';
 
-var app = angular.module( 'renderteam.collapsibleTree', []);
-app.directive( 'collapsibleTree', collapsibleTree );
+ var app = angular.module('renderteam.collapsibleTree', []);
+ app.directive('collapsibleTree', collapsibleTree);
 
-collapsibleTree.$inject = [];
-function collapsibleTree () {
-  return {
-    restrict: 'E',
-    scope: {
-      width:  '=',
-      height: '=',
-      radius: '=',
-      nodes:  '='
-    },
-    link: function ( scope, element ) {
-      var m = [20, 120, 20, 120],
-          w = 1280 - m[1] - m[3],
-          h = 800 - m[0] - m[2],
-          i = 0,
-          root;
+ collapsibleTree.$inject = [];
 
-      var tree = d3.layout.tree()
-          .size([h, w]);
+ function collapsibleTree() {
+     return {
+         restrict: 'E',
+         scope: {
+             width: '=',
+             height: '=',
+             radius: '=',
+             nodes: '='
+         },
+         link: function(scope, element) {
+             var m = [20, 120, 20, 120],
+                 w = 1400 - m[1] - m[3],
+                 h = 1000 - m[0] - m[2],
+                 i = 0,
+                 root;
 
-      var diagonal = d3.svg.diagonal()
-          .projection(function(d) { return [d.y, d.x]; });
+             var tree = d3.layout.tree()
+                 .size([h, w]);
 
-      var vis = d3.select("collapsible-tree").append("svg:svg")
-          .attr("width", w + m[1] + m[3])
-          .attr("height", h + m[0] + m[2])
-        .append("svg:g")
-          .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+             var diagonal = d3.svg.diagonal()
+                 .projection(function(d) {
+                     return [d.y, d.x];
+                 });
+
+             var vis = d3.select("collapsible-tree").append("svg:svg")
+                 .attr("width", w + m[1] + m[3])
+                 .attr("height", h + m[0] + m[2])
+                 .append("svg:g")
+                 .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
 
 
-      scope.$watch('nodes', function () {
+             scope.$watch('nodes', function() {
 
-        function toggleAll(d) {
-          if (d.children) {
-            d.children.forEach(toggleAll);
-            toggle(d);
-          }
-        }
-        if ( scope.nodes ) {
-          root = angular.copy(scope.nodes);
-          root.x0 = h / 2;
-          root.y0 = 0;
+                 function toggleAll(d) {
+                     if (d.children) {
+                         d.children.forEach(toggleAll);
+                         toggle(d);
+                     }
+                 }
+                 if (scope.nodes) {
+                     root = angular.copy(scope.nodes);
+                     root.x0 = h / 2;
+                     root.y0 = 0;
 
-          // Initialize the display to show a few nodes.
-          root.children.forEach(toggleAll);
-          toggle(root.children[1]);
-          toggle(root.children[1].children[2]);
-          toggle(root.children[9]);
-          toggle(root.children[9].children[0]);
+                     // Initialize the display to show a few nodes.
+                     root.children.forEach(toggleAll);
 
-          update(root);
-        }
-      }, true);
+                     update(root);
+                 }
+             }, true);
 
-      function update(source) {
-        var duration = d3.event && d3.event.altKey ? 5000 : 500;
+             function wrap(text, width) {
+                 text.each(function() {
+                     var text = d3.select(this),
+                         words = text.text().split(/\s+/).reverse(),
+                         word,
+                         line = [],
+                         lineNumber = 0,
+                         lineHeight = 1.1, // ems
+                         y = text.attr("y"),
+                         dy = parseFloat(text.attr("dy")),
+                         tspan = text.text(null).append("tspan").attr("x", text.attr("x")).attr("y", y).attr("dy", dy + "em");
+                     while (word = words.pop()) {
+                         line.push(word);
+                         tspan.text(line.join(" "));
+                         if (tspan.node().getComputedTextLength() > width) {
+                             line.pop();
+                             tspan.text(line.join(" "));
+                             line = [word];
+                             tspan = text.append("tspan").attr("x", text.attr("x")).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+                         }
+                     }
+                 });
+             }
 
-        // Compute the new tree layout.
-        var nodes = tree.nodes(root).reverse();
+             function update(source) {
+                 var duration = d3.event && d3.event.altKey ? 5000 : 500;
 
-        // Normalize for fixed-depth.
-        nodes.forEach(function(d) { d.y = d.depth * 180; });
+                 // Compute the new tree layout.
+                 var nodes = tree.nodes(root).reverse();
 
-        // Update the nodes
-        var node = vis.selectAll("g.node")
-            .data(nodes, function(d) { return d.id || (d.id = ++i); });
+                 // Normalize for fixed-depth.
+                 nodes.forEach(function(d) {
+                     d.y = d.depth * 240;
+                 });
 
-        // Enter any new nodes at the parent's previous position.
-        var nodeEnter = node.enter().append("svg:g")
-            .attr("class", "node")
-            .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
-            .on("click", function(d) { toggle(d); update(d); });
+                 // Update the nodes
+                 var node = vis.selectAll("g.node")
+                     .data(nodes, function(d) {
+                         return d.id || (d.id = ++i);
+                     });
 
-        nodeEnter.append("svg:circle")
-            .attr("r", 1e-6)
-            .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+                 // Enter any new nodes at the parent's previous position.
+                 var nodeEnter = node.enter().append("svg:g")
+                     .attr("class", "node")
+                     .attr("transform", function(d) {
+                         return "translate(" + source.y0 + "," + source.x0 + ")";
+                     })
+                     .on("click", function(d) {
+                         toggle(d);
+                         update(d);
+                     });
 
-        nodeEnter.append("svg:text")
-            .attr("x", function(d) { return d.children || d._children ? -10 : 10; })
-            .attr("dy", ".35em")
-            .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
-            .text(function(d) { return d.name; })
-            .style("fill-opacity", 1e-6);
+                 nodeEnter.append("svg:circle")
+                     .attr("r", 1e-6)
+                     .style("fill", function(d) {
+                         return d._children ? "lightsteelblue" : "#fff";
+                     });
 
-        // Transition nodes to their new position.
-        var nodeUpdate = node.transition()
-            .duration(duration)
-            .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+                 var t = nodeEnter.append("svg:text")
+                     .attr("x", function(d) {
+                         return d.children || d._children ? -10 : 10;
+                     })
+                     .attr("dy", ".35em")
+                     .attr("text-anchor", function(d) {
+                         return d.children || d._children ? "end" : "start";
+                     })
+                     .text(function(d) {
+                         return d.name;
+                     })
+                     .style("fill-opacity", 1e-6);
 
-        nodeUpdate.select("circle")
-            .attr("r", 4.5)
-            .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+                 wrap(t, 230);
 
-        nodeUpdate.select("text")
-            .style("fill-opacity", 1);
+                 // Transition nodes to their new position.
+                 var nodeUpdate = node.transition()
+                     .duration(duration)
+                     .attr("transform", function(d) {
+                         return "translate(" + d.y + "," + d.x + ")";
+                     });
 
-        // Transition exiting nodes to the parent's new position.
-        var nodeExit = node.exit().transition()
-            .duration(duration)
-            .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
-            .remove();
+                 nodeUpdate.select("circle")
+                     .attr("r", 4.5)
+                     .style("fill", function(d) {
+                         return d._children ? "lightsteelblue" : "#fff";
+                     });
 
-        nodeExit.select("circle")
-            .attr("r", 1e-6);
+                 nodeUpdate.select("text")
+                     .style("fill-opacity", 1);
 
-        nodeExit.select("text")
-            .style("fill-opacity", 1e-6);
+                 // Transition exiting nodes to the parent's new position.
+                 var nodeExit = node.exit().transition()
+                     .duration(duration)
+                     .attr("transform", function(d) {
+                         return "translate(" + source.y + "," + source.x + ")";
+                     })
+                     .remove();
 
-        // Update the links
-        var link = vis.selectAll("path.link")
-            .data(tree.links(nodes), function(d) { return d.target.id; });
+                 nodeExit.select("circle")
+                     .attr("r", 1e-6);
 
-        // Enter any new links at the parent's previous position.
-        link.enter().insert("svg:path", "g")
-            .attr("class", "link")
-            .attr("d", function(d) {
-              var o = {x: source.x0, y: source.y0};
-              return diagonal({source: o, target: o});
-            })
-          .transition()
-            .duration(duration)
-            .attr("d", diagonal);
+                 nodeExit.select("text")
+                     .style("fill-opacity", 1e-6);
 
-        // Transition links to their new position.
-        link.transition()
-            .duration(duration)
-            .attr("d", diagonal);
+                 // Update the links
+                 var link = vis.selectAll("path.link")
+                     .data(tree.links(nodes), function(d) {
+                         return d.target.id;
+                     });
 
-        // Transition exiting nodes to the parent's new position.
-        link.exit().transition()
-            .duration(duration)
-            .attr("d", function(d) {
-              var o = {x: source.x, y: source.y};
-              return diagonal({source: o, target: o});
-            })
-            .remove();
+                 // Enter any new links at the parent's previous position.
+                 link.enter().insert("svg:path", "g")
+                     .attr("class", "link")
+                     .attr("d", function(d) {
+                         var o = {
+                             x: source.x0,
+                             y: source.y0
+                         };
+                         return diagonal({
+                             source: o,
+                             target: o
+                         });
+                     })
+                     .transition()
+                     .duration(duration)
+                     .attr("d", diagonal);
 
-        // Stash the old positions for transition.
-        nodes.forEach(function(d) {
-          d.x0 = d.x;
-          d.y0 = d.y;
-        });
-      }
+                 // Transition links to their new position.
+                 link.transition()
+                     .duration(duration)
+                     .attr("d", diagonal);
 
-      // Toggle children.
-      function toggle(d) {
-        if (d.children) {
-          d._children = d.children;
-          d.children = null;
-        } else {
-          d.children = d._children;
-          d._children = null;
-        }
-      }
+                 // Transition exiting nodes to the parent's new position.
+                 link.exit().transition()
+                     .duration(duration)
+                     .attr("d", function(d) {
+                         var o = {
+                             x: source.x,
+                             y: source.y
+                         };
+                         return diagonal({
+                             source: o,
+                             target: o
+                         });
+                     })
+                     .remove();
 
-    }
+                 // Stash the old positions for transition.
+                 nodes.forEach(function(d) {
+                     d.x0 = d.x;
+                     d.y0 = d.y;
+                 });
+             }
 
-  };
-}
+             // Toggle children.
+             function toggle(d) {
+                 if (d.children && (d.children.length == 0 || d.children === null)) delete d.children;
+                 if (d._children && (d._children.length == 0 || d._hildren === null)) delete d._children;
+                 if (d.children) {
+                     d._children = d.children;
+                     d.children = null;
+                 } else {
+                     d.children = d._children;
+                     d._children = null;
+                 }
+             }
+
+         }
+
+     };
+ }
